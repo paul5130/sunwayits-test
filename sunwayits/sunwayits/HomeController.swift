cc097650d1a8052d529d7dea8b699605f89aa071
paul5130 <paul51305040@gmail.com>
2024-12-31 17:01:53 +0800

    Initialize the project

---
+++ sunwayits/sunwayits/HomeController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeController.swift
+//  sunwayits
+//
+//  Created by Paul Wen on 2024/12/31.
+//
+
+import UIKit
+
+class HomeController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
